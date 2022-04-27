1162f4f8877f2165f44804168ad284c0255f907e
Adam Crespo <alc49@LAPTOP-7KRCGAGF.localdomain>
2022-04-27 13:33:09 -0400

    commit tree 829989fd0a928a8c337b673dfba6a6d613e623f4

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,4 +1,5 @@
 class DogHousesController < ApplicationController
+  before_action :find_dog_house
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
@@ -6,10 +7,24 @@ class DogHousesController < ApplicationController
     render json: dog_house
   end
 
+  def reviews_index
+    reviews = @dog_house.reviews
+    render json: reviews, include: :dog_house, status: 200
+  end
+
+  def review
+    review = Review.find(params[:id])
+    render json: review, include: :dog_house, status: 200
+  end
+
   private
 
   def render_not_found_response
     render json: { error: "Dog house not found" }, status: :not_found
+  end
+
+  def find_dog_house 
+    @dog_house = DogHouse.find(params[:dog_house_id])
   end
 
 end
